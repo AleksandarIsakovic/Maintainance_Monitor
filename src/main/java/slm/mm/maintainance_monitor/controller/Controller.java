f5959189d38493e36acc47d213e1067c03ef86bc
AleksandarIsakovic <ic22b074@technikum-wien.at>
2023-01-10 21:14:28 +0100

    test-commit

--- src/main/java/slm/mm/maintainance_monitor/controller/Controller.java
+++ src/main/java/slm/mm/maintainance_monitor/controller/Controller.java
@@ -7,25 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-    private String maintainceMessage = "Everything works as expected";
-    @GetMapping("/api/message")
-    public String message(){
-        return maintainceMessage;
 
-    }
-    @PostMapping("/api/message/set")
-    public String setmaintance(@RequestParam String param){
-        if(param != null){
-        maintainceMessage = param;
-        return "ok";
-        }else{
-            return "Error while setting Maintaince ";
-        }
 
-    }
-    @PostMapping("/api/message/reset")
-    public String reset(){
-        maintainceMessage= "Everything works as expected";
-        return "ok";
-    }
+
+
 }
