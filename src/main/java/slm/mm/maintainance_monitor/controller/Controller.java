a55f2978575fc5a29ec627e217068499eca65fc8
CNR24 <103997334+CNR24@users.noreply.github.com>
2023-01-13 16:24:45 +0100

    This commit make update for Controller.java

--- src/main/java/slm/mm/maintainance_monitor/controller/Controller.java
+++ src/main/java/slm/mm/maintainance_monitor/controller/Controller.java
@@ -1,4 +1,5 @@
-package slm.mm.maintainance_monitor.controller;
+package at.fhtw.bic3.g9.maintanance_monitor2;
+
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,8 +8,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+    private String maintainceMessage = "Everything works as expected";
+    @GetMapping("/api/message")
+    public String message(){
+        return maintainceMessage;
+    }
+    @PostMapping("/api/message/set")
+    public String setmaintance(@RequestParam String param){
+        if(param != null){
+            maintainceMessage = param;
+            return "Reset is ok!";
+        }else{
+            return "Error while setting Maintaince ";
+        }
 
-
-
-
+    }
+    @GetMapping("/api/message/reset")
+    public String reset(){
+        maintainceMessage= "Messege reset worked as expected";
+        return maintainceMessage;
+    }
 }
